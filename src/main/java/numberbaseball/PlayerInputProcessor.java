952b694539969f8ad0587f599edd2f8e3691d3a2
Rose <82176176+geonwoo0215@users.noreply.github.com>
2022-07-15 04:06:56 +0900

    feat : 사용자 입력 Integer 타입으로 변환

--- src/main/java/numberbaseball/PlayerInputProcessor.java
+++ src/main/java/numberbaseball/PlayerInputProcessor.java
@@ -3,30 +3,39 @@ package numberbaseball;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.Scanner;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class PlayerInputProcessor {
 
     private final int NUMBER_DIGIT = 3;
     private final BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-    private LinkedList<Integer> playerInput = new LinkedList<>();
-    private char[] input = new char[NUMBER_DIGIT];
-
+    private List<Integer> playerInput = new LinkedList<>();
+    private String input;
 
     public void getPlayerInput() throws IOException {
-        input = bf.readLine().toCharArray();
+        input = bf.readLine();
     }
 
     public boolean isNumber() {
-        for (char num : input){
-            if(num<'0'||num>'9'){
-                return false;
-            }
-        }
-        return true;
+       try{
+           Integer.parseInt(input);
+       }catch (Exception e){
+           return false;
+       }
+       return true;
     }
+
+    public void toIntegerList(){
+        playerInput = Arrays.stream(input.split(""))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+
 
 
 }
