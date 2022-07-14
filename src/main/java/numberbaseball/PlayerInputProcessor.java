b69ded6c3e41946b81045cad969615c469438f67
Rose <82176176+geonwoo0215@users.noreply.github.com>
2022-07-15 03:27:52 +0900

    feat : 사용자 입력 숫자 검증 메서드

--- src/main/java/numberbaseball/PlayerInputProcessor.java
+++ src/main/java/numberbaseball/PlayerInputProcessor.java
@@ -8,11 +8,24 @@ import java.util.Scanner;
 
 public class PlayerInputProcessor {
 
-    private LinkedList<Integer> playerInput = new LinkedList<>();
+    private final int NUMBER_DIGIT = 3;
     private final BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
+    private LinkedList<Integer> playerInput = new LinkedList<>();
+    private char[] input = new char[NUMBER_DIGIT];
+
+
     public void getPlayerInput() throws IOException {
-        char[] input = bf.readLine().toCharArray();
+        input = bf.readLine().toCharArray();
+    }
+
+    public boolean isNumber() {
+        for (char num : input){
+            if(num<'0'||num>'9'){
+                return false;
+            }
+        }
+        return true;
     }
 
 
