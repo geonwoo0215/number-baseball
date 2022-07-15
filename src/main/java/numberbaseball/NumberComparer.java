b74d266facf0cd4400c62aae66f12d48497dc23b
Rose <82176176+geonwoo0215@users.noreply.github.com>
2022-07-15 17:49:04 +0900

    feat(NumberComparer) : 스트라이크 판정 메서드 추가

--- src/main/java/numberbaseball/NumberComparer.java
+++ src/main/java/numberbaseball/NumberComparer.java
@@ -7,6 +7,9 @@ public class NumberComparer {
     private List<Integer> randomNumber = new LinkedList<>();
     private List<Integer> playerInput = new LinkedList<>();
 
+    public int strike;
+    public int ball;
+
     public void setRandomNumber(List<Integer> randomNumber) {
         this.randomNumber = randomNumber;
     }
@@ -14,4 +17,17 @@ public class NumberComparer {
     public void setPlayerInput(List<Integer> playerInput) {
         this.playerInput = playerInput;
     }
+
+    public void isStrike(){
+        strike = 0;
+        for(int i : playerInput){
+            if(randomNumber.contains(i)&&playerInput.indexOf(i)==randomNumber.indexOf(i)){
+                strike++;
+            }
+        }
+    }
+
+
+
+
 }
