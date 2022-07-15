71640f159f2175ad61b204b2c13128ce0a3a7658
Rose <82176176+geonwoo0215@users.noreply.github.com>
2022-07-15 21:23:35 +0900

    feat(MessageManager) : 메시지 관리 클래스 추가

---
+++ src/main/java/numberbaseball/MessageManger.java
@@ -0,0 +1,9 @@
+package numberbaseball;
+
+public class MessageManger {
+
+    private final String INPUT_REQUEST = "숫자를 입력해주세요";
+    private final String ANSWER = "3개의 숫자를 모두 맞혔습니다.!";
+    private final String RETRY = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+}
