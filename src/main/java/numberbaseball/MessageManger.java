b2fa772cc977c2228760a0184797ff831bde967a
Rose <82176176+geonwoo0215@users.noreply.github.com>
2022-07-16 19:47:52 +0900

    feat(MessageManager) : 스트라이크, 볼 출력 메서드 추가

--- src/main/java/numberbaseball/MessageManger.java
+++ src/main/java/numberbaseball/MessageManger.java
@@ -10,15 +10,18 @@ public class MessageManger {
     private final String ANSWER_MESSAGE = "3개의 숫자를 모두 맞혔습니다.! 게임 종료";
     private final String RETRY_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private final String WARNING_MESSAGE = "잘못된 입력입니다.";
+    private final String BALL = "볼 ";
+    private final String STRIKE = "스트라이크";
+    private final String NOTHING = "낫싱";
     private BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
     private PlayerInputProcessor playerInputProcessor = new PlayerInputProcessor();
-
+    private NumberComparer numberComparer = new NumberComparer();
 
     public String input() throws IOException {
         return bf.readLine();
     }
 
-    private void inputRequest() throws IOException {
+    public void inputRequest() throws IOException {
 
         try{
             System.out.println(INPUT_MESSAGE);
@@ -30,20 +33,32 @@ public class MessageManger {
 
     }
 
-    private void retryRequest() throws IOException {
+    public void retryRequest() throws IOException {
+
         try {
             System.out.println(ANSWER_MESSAGE);
             System.out.println(RETRY_MESSAGE);
-            playerInputProcessor.isOneOrTwo(input());
+            playerInputProcessor.validateAnswerNumber(input());
         } catch (Exception e){
             System.out.println(WARNING_MESSAGE);
             retryRequest();
         }
 
-
     }
 
-
-
+    public void inputResponse(){
+        int ball = numberComparer.getBall();
+        int strike = numberComparer.getStrike();
+        StringBuilder sb = new StringBuilder();
+        if(ball>0){
+            sb.append(ball).append(BALL);
+        }
+        if(strike>0){
+            sb.append(strike).append(STRIKE);
+        }
+        if(strike==0&&ball==0){
+            sb.append(NOTHING);
+        }
+    }
 
 }
