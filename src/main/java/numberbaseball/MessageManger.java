118892ab22b5474a32f39f7702ae027ac1cba1ce
Rose <82176176+geonwoo0215@users.noreply.github.com>
2022-07-16 18:35:04 +0900

    feat(MessageManager) : 사용자 입력 처리 메서드 추가

--- src/main/java/numberbaseball/MessageManger.java
+++ src/main/java/numberbaseball/MessageManger.java
@@ -1,9 +1,31 @@
 package numberbaseball;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class MessageManger {
 
-    private final String INPUT_REQUEST = "숫자를 입력해주세요";
-    private final String ANSWER = "3개의 숫자를 모두 맞혔습니다.!";
-    private final String RETRY = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    private final String ANSWER_MESSAGE = "3개의 숫자를 모두 맞혔습니다.! 게임 종료";
+    private final String RETRY_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    private PlayerInputProcessor playerInputProcessor = new PlayerInputProcessor();
+
+
+    private void inputRequest() throws IOException {
+
+        try{
+            System.out.println(INPUT_MESSAGE);
+            String playerInput = bf.readLine();
+            playerInputProcessor.validateNumber(playerInput);
+        } catch (Exception e){
+            inputRequest();
+        }
+
+    }
+
+
+
 
 }
