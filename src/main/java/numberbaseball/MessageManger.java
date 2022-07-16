1a4c55d2e5d583432432aaa43ad21e32f87f4290
Rose <82176176+geonwoo0215@users.noreply.github.com>
2022-07-16 19:11:23 +0900

    feat(MessageManager) : 재시작 입력 요청 메서드 추가

--- src/main/java/numberbaseball/MessageManger.java
+++ src/main/java/numberbaseball/MessageManger.java
@@ -9,19 +9,37 @@ public class MessageManger {
     private final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
     private final String ANSWER_MESSAGE = "3개의 숫자를 모두 맞혔습니다.! 게임 종료";
     private final String RETRY_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private final String WARNING_MESSAGE = "잘못된 입력입니다.";
     private BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
     private PlayerInputProcessor playerInputProcessor = new PlayerInputProcessor();
 
+
+    public String input() throws IOException {
+        return bf.readLine();
+    }
 
     private void inputRequest() throws IOException {
 
         try{
             System.out.println(INPUT_MESSAGE);
-            String playerInput = bf.readLine();
-            playerInputProcessor.validateNumber(playerInput);
+            playerInputProcessor.validateNumber(input());
         } catch (Exception e){
+            System.out.println(WARNING_MESSAGE);
             inputRequest();
         }
+
+    }
+
+    private void retryRequest() throws IOException {
+        try {
+            System.out.println(ANSWER_MESSAGE);
+            System.out.println(RETRY_MESSAGE);
+            playerInputProcessor.isOneOrTwo(input());
+        } catch (Exception e){
+            System.out.println(WARNING_MESSAGE);
+            retryRequest();
+        }
+
 
     }
 
